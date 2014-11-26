162a857f2324a66a10eee4a67486a95e4c6db40e
Chi Kei Chan <chikeichan@gmail.com>
2014-11-26 12:25:42 -0800

    Added functions to move player to get ball

--- basketball.js
+++ basketball.js
@@ -17,7 +17,8 @@ bb.court = {
 	boardSpace: 4,
 	boardLength: 6,
 	penaltyWidth: 1.25,
-	penaltyRadius: 4
+	penaltyRadius: 4,
+	ballRadius: 0.75
 }
 
 bb.matchRatio = function(){
@@ -133,19 +134,84 @@ bb.drawCourt = function(c,ctx){
 	ctx.arc((bb.court.width-bb.court.paintWidth),bb.court.height/2,bb.court.outerRadius,-Math.PI/2,-1.5*Math.PI);
 	ctx.stroke();
 	ctx.setLineDash([0]);
+	ctx.translate(-(c.width-bb.court.width)/2,-(c.height-bb.court.height)/2);
 }
 
+//Speed is feet per 30 milliseconds
+bb.player = {
+	name: 'Jacky',
+	speed: 0.75
+}
 
+bb.drawBall = function(canvas,context,x,y){
+	context.translate((canvas.width-bb.court.width)/2,(canvas.height-bb.court.height)/2);
+	context.fillStyle = 'rgb(184, 82, 48)'
+	context.beginPath();
+	context.arc(x*bb.ratio,y*bb.ratio,bb.court.ballRadius,0,2*Math.PI)
+	context.fill();
+	context.translate(-(canvas.width-bb.court.width)/2,-(canvas.height-bb.court.height)/2);
+}
 
+bb.drawPlayer = function(canvas,context,x,y){
+	context.translate((canvas.width-bb.court.width)/2,(canvas.height-bb.court.height)/2);
+	context.fillStyle = 'blue'
+	context.beginPath();
+	context.arc(x*bb.ratio,y*bb.ratio,1*bb.ratio,0,2*Math.PI);
+	context.fill();
+	
+	context.translate(-(canvas.width-bb.court.width)/2,-(canvas.height-bb.court.height)/2);
+}
 
+bb.playerPos = {
+	x: 44,
+	y: 25
+}
 
+bb.ballPos = {
+	x: 19,
+	y: 25
+}
 
+bb.possession = null;
 
+bb.getBall = function(){
+	var dX = bb.ballPos.x - bb.playerPos.x;
+	var dY = bb.ballPos.y - bb.playerPos.y;
 
+	if(dX < 0){
+		bb.playerPos.x = bb.playerPos.x - bb.player.speed;
+	} else if (dX > 0){
+		bb.playerPos.x = bb.playerPos.x + bb.player.speed;
+	}
 
+	if(dY < 0){
+		bb.playerPos.y = bb.playerPos.y - bb.player.speed;
+	} else if (dY > 0){
+		bb.playerPos.y = bb.playerPos.y + bb.player.speed;
+	}
 
+	if(dX < bb.player.speed && dX > -bb.player.speed){
+		bb.playerPos.x = bb.ballPos.x;
+	}
 
+	if(dY < bb.player.speed && dY > -bb.player.speed){
+		bb.playerPos.y = bb.ballPos.y;
+	}
 
+	if(dX === 0 && dY === 0){
+		bb.possession = 'player'
+	}
+
+	console.log(bb.playerPos)
+
+}
+
+bb.render = function(c,ctx){
+	ctx.clearRect(0,0,c.width,c.height);
+	bb.drawCourt(c,ctx);
+	bb.drawBall(c,ctx,bb.ballPos.x,bb.ballPos.y);
+	bb.drawPlayer(c,ctx,bb.playerPos.x,bb.playerPos.y);
+}
 
 
 
@@ -158,11 +224,16 @@ $(document).ready(function(){
 	bb.matchRatio();
 	var c = document.getElementById('court');
 	var ctx = c.getContext('2d');
-	bb.drawCourt(c,ctx);
+	bb.render(c,ctx);
 
-	ctx.fillStyle = 'rgb(184, 82, 48)'
-	ctx.beginPath();
-	ctx.arc(bb.court.width/2,bb.court.height/2,0.75*8.2,0,2*Math.PI)
-	ctx.fill();
+	var start = setInterval(function(){
+		if(!bb.possession){
+			bb.getBall();
+			bb.render(c,ctx);
+		} else {
+			clearInterval(start);
+		}
+	},30)
+
 
 })
